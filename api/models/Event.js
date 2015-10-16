db7bec2718f6d4250df99805e94638d68467d75e
SylvainLap <sylvain.lapendry@gmail.com>
2015-10-16 16:25:58 +0200

    added Event API

---
+++ api/models/Event.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  attributes: {
+    name: {
+      type: 'string',
+      required: true
+    },
+    country: {
+      type: 'string'
+    },
+    town: {
+      type: 'string'
+    },
+    date: {
+      type: 'string'
+    }
+  }
+
+};
